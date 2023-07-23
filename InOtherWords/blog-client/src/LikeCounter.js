1f73e26471acc338209cef3edcb020036fc7f57e
Dassi Rosenthal <dassirosenthal@gmail.com>
2023-07-23 14:19:05 -0400

    blog-client

--- InOtherWords/blog-client/src/LikeCounter.js
+++ InOtherWords/blog-client/src/LikeCounter.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 export default async function LikeCounter({ type, parent }) {
     let imgsrc;
     type === 'like' ? imgsrc = 'like-emoji.png' : imgsrc = 'dislike-emoji.png';
-    const previousCount = await fetch();
+    const previousCount = 0 //await fetch() from db;
     const [count, setCount] = useState(previousCount);
     function countClicks() {
         setCount(count + 1);
