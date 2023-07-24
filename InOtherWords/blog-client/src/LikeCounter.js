bd6b7d87f5646a7c2e6e569e2b04d46909bdbd24
DassiRosenthal <106287450+DassiRosenthal@users.noreply.github.com>
2023-07-24 14:47:35 -0400

    blog-client

--- InOtherWords/blog-client/src/LikeCounter.js
+++ InOtherWords/blog-client/src/LikeCounter.js
@@ -1,9 +1,9 @@
-import React/*, { useState }*/ from 'react';
+import React, { useState } from 'react';
 
 export default function LikeCounter({ type, prevCount, postId, setError }) {
     let imgsrc;
     type === 'like' ? imgsrc = 'like-emoji.png' : imgsrc = 'dislike-emoji.png';
-    //const [count, setCount] = useState(prevCount);
+    const [count, setCount] = useState(prevCount);
     async function countClicks() {
         try {
             const response = await fetch(`https://inotherwords-api.onrender.com/addLike/${postId}`, {
@@ -12,7 +12,7 @@ export default function LikeCounter({ type, prevCount, postId, setError }) {
                     'Content-Type': 'application/json'
                 },
                 credentials: 'include',
-                body: JSON.stringify({ 'type': type, 'count': prevCount + 1 })
+                body: JSON.stringify({ 'type': type, 'count': count + 1 })
             });
             if (!response.ok) {
                 if (response.status === 401) {
@@ -21,7 +21,7 @@ export default function LikeCounter({ type, prevCount, postId, setError }) {
                 const errorText = await response.text();
                 throw new Error(errorText);
             }
-            //setCount(count + 1);
+            setCount(count + 1);
         } catch (e) {
             setError(e.message);
         }
@@ -30,7 +30,7 @@ export default function LikeCounter({ type, prevCount, postId, setError }) {
         <>
             <button className='like-button' onClick={countClicks}>
                 <img className='like-emoji' src={imgsrc} alt={type}></img>
-                <span className={type + '-count'}>{prevCount}</span>
+                <span className={type + '-count'}>{count}</span>
             </button>
 
         </>
