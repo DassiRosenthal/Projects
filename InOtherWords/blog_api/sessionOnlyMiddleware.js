700792575d39904c8c7e8063752952393ab21fc5
Dassi Rosenthal <dassirosenthal@gmail.com>
2023-08-09 19:27:09 -0400

    blog_api

--- InOtherWords/blog_api/sessionOnlyMiddleware.js
+++ InOtherWords/blog_api/sessionOnlyMiddleware.js
@@ -2,8 +2,9 @@ module.exports = function sessionOnlyMiddleware(req, res, next) {
   console.log('in sessionOnlyMiddleware');
   console.log(req.session)
   console.log('_______________________')
-  console.log(req.session.user.username);
-   if (req.session.user.username) {
+  //console.log(req.session.user.username);
+  //if (req.session.user.username) {
+  if (req.session) {
     next();
   } else {
     res.sendStatus(401);
